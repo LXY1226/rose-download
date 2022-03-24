da011cc452e9b2efe734d6cdbc3d3e09b7d1a54c
Lin <767763591@qq.com>
2022-03-24 20:35:31 +0800

    style: not goto

--- proxy.go
+++ proxy.go
@@ -41,47 +41,49 @@ func runProxy(ip net.IP) {
 	var conn *net.TCPConn
 	for curTask != nil {
 		curTask.Do(curTask.init)
-		goto start
-	_err:
-		conn.Close()
-		if err != ErrNext {
-			log.Println(ip, err)
-			time.Sleep(30 * time.Second)
+		for {
+			if err != nil {
+				conn.Close()
+				if err != ErrNext {
+					log.Println(ip, err)
+					time.Sleep(30 * time.Second)
+				}
+				time.Sleep(5 * time.Second) // 等待前一连接正常关闭
+			}
+			conn, err = net.DialTCP("tcp", nil, &net.TCPAddr{IP: ip, Port: 443})
+			if err != nil {
+				continue
+			}
+			conn.SetReadBuffer(64 << 10)
+			br := bufio.NewReaderSize(conn, 256)
+			taskMutex.Lock()
+			task := curTask
+			taskMutex.Unlock()
+			err = task.header.SendProxyHeader(conn)
+			if err != nil {
+				err = errors.New("squid代理失败 " + err.Error())
+				continue
+			}
+			stat, err = readHead(br)
+			if stat != 200 {
+				err = fmt.Errorf("squid响应无效 %d %s", stat, err.Error())
+				continue
+			}
+			err = task.Go(conn, br)
+			if err != nil {
+				continue
+			}
+			conn.Close()
+			time.Sleep(2 * time.Second) // 等待前一连接正常关闭
+			taskMutex.Lock()
+			if task == curTask {
+				log.Println(curTask.filename, "任务完成")
+				curTask.SaveStat()
+				nextTask()
+			}
+			taskMutex.Unlock()
+			break
 		}
-		time.Sleep(5 * time.Second) // 等待前一连接正常关闭
-	start:
-		conn, err = net.DialTCP("tcp", nil, &net.TCPAddr{IP: ip, Port: 443})
-		if err != nil {
-			goto _err
-		}
-		conn.SetReadBuffer(64 << 10)
-		br := bufio.NewReaderSize(conn, 256)
-		taskMutex.Lock()
-		task := curTask
-		taskMutex.Unlock()
-		err = task.header.SendProxyHeader(conn)
-		if err != nil {
-			err = errors.New("squid代理失败 " + err.Error())
-			goto _err
-		}
-		stat, err = readHead(br)
-		if stat != 200 {
-			err = fmt.Errorf("squid响应无效 %d %s", stat, err.Error())
-			goto _err
-		}
-		err = task.Go(conn, br)
-		if err != nil {
-			goto _err
-		}
-		conn.Close()
-		time.Sleep(2 * time.Second) // 等待前一连接正常关闭
-		taskMutex.Lock()
-		if task == curTask {
-			log.Println(curTask.filename, "任务完成")
-			curTask.SaveStat()
-			nextTask()
-		}
-		taskMutex.Unlock()
 	}
 	wg.Done()
 }
